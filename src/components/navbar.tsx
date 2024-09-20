6529f331cdf245ba437615bb4e788ec508896c0c
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2024-09-20 10:02:41 +0700

    init ui config

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,13 @@
+import { Button } from "./ui/button";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      Navbar
+      <Button>Please Click Here</Button>
+      <Button variant="outline">Please Click Here</Button>
+      <Button variant="link">Please Click Here</Button>
+      <Button variant="secondary">Please Click Here</Button>
+    </div>
+  );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,29 +1,26 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md body1 font-medium transition-colors",
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+        default: "bg-primary-700 text-neutral-50 hover:bg-primary-700/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-primary-500 text-primary-500 shadow-sm hover:bg-primary-500 hover:text-neutral-50",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-primary-500 text-neutral-50 shadow-sm hover:bg-primary-500/80",
+        link: "text-primary-500 underline-offset-4 underline decoration-primary-500",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
+        sm: "body2 h-[20px] px-[8px] py-[4px] lg:h-[34px] lg:px[16px] lg:py-[8px]",
+        default:
+          "h-[28px] px-[12px] py-[6px] lg:h-[40px] lg:px[20px] lg:py-[10px]",
+        lg: "h-[36px] px-[20px] py-[10px]",
         icon: "h-9 w-9",
       },
     },
@@ -32,26 +29,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
